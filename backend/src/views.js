98dd9c471a2a8bfeee4e4b875f58cd44a16251c6
Dmitry Chermnykh <27727194+Chermnyx@users.noreply.github.com>
2018-11-15 16:12:21 +0300

    Add validator wrapper

--- backend/src/views.js
+++ backend/src/views.js
@@ -1,3 +1,28 @@
+const Joi = require('joi');
 const router = require('express').Router();
+
+const errors = require('./errors');
+
+function asyncHandler(fn) {
+  return (req, res, next) => {
+    fn(req, res, next).catch((err) => next(err));
+  };
+}
+
+function validate(object, schema) {
+  const { error, value } = Joi.validate(object, schema, {
+    abortEarly: false,
+    presence: 'required',
+    skipFunctions: true,
+    allowUnknown: true,
+    stripUnknown: true,
+  });
+
+  if (error) {
+    throw new errors.ValidationError(error);
+  }
+
+  return value;
+}
 
 module.exports = router;
